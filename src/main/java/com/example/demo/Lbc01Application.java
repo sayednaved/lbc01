9cc4332beee04283239e5423117631e66e97bdba
Sayed Naved <sayednaved@gmail.com>
2017-08-05 10:43:26 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Lbc01Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lbc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lbc01Application.class, args);
+	}
+}
